40f872476842573c8ce1b37105dab056aac0422d
unknown <rambudiarto@gmail.com>
2025-02-24 17:12:26 +0700

    fix: create conditional button

--- model-spawn.js
+++ model-spawn.js
@@ -2,8 +2,11 @@ AFRAME.registerComponent('model-spawn', {
   init() {
     const {el} = this
     const video = document.querySelectorAll('#video')
+    const text = document.querySelector('.already-found-text')
     el.sceneEl.addEventListener('xrimagelost', () => {
       el.object3D.visible = true
+
+      text.style.opacity = '0'
 
       // Buat model
       const model = document.createElement('a-entity')
